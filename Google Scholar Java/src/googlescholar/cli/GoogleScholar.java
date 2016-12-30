0f5fee14e3a810e4f01f49ba9498a7f7ca094bcc
Gustavo Silva <silva95gustavo@gmail.com>
2016-12-30 01:56:22 +0000

    Logged in menu.

--- Google Scholar Java/src/googlescholar/cli/GoogleScholar.java
+++ Google Scholar Java/src/googlescholar/cli/GoogleScholar.java
@@ -1,7 +1,11 @@
 package googlescholar.cli;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
+import googlescholar.model.Paper;
 import googlescholar.model.Scholar;
 
 public class GoogleScholar {
@@ -19,7 +23,7 @@ public class GoogleScholar {
 		}
 	}
 	
-	public void mainMenu() {
+	private void mainMenu() {
 		System.out.println("1 - Register");
 		System.out.println("2 - Login");
 		System.out.println();
@@ -34,7 +38,7 @@ public class GoogleScholar {
 		}
 	}
 	
-	public void register() {
+	private void register() {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Email: ");
 		String email = s.nextLine();
@@ -46,7 +50,7 @@ public class GoogleScholar {
 		mainMenu();
 	}
 	
-	public void login() {
+	private void login() {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Email: ");
 		String email = s.nextLine();
@@ -63,7 +67,72 @@ public class GoogleScholar {
 		}
 	}
 	
-	public void userMenu() {
+	private void userMenu() {
+		System.out.println("Hello, " + scholar.getCurrentUser().getEmail());
+		System.out.println();
+		System.out.println("1 - Change password");
+		System.out.println("2 - Search paper");
+		System.out.println("3 - Add paper");
+		System.out.println();
+		System.out.println("0 - Logout");
+		
+		Scanner s = new Scanner(System.in);
+		int choice = s.nextInt();
+		switch (choice) {
+		case 1: changePassword(); break;
+		case 2: searchPaper(); break;
+		case 3: addPaper(); break;
+		case 0: logout(); break;
+		default: userMenu();
+		}
+	}
+	
+	private void logout() {
+		scholar.logout();
+		mainMenu();
+	}
+	
+	private void changePassword() {
+		System.out.print("New password: ");
+		Scanner s = new Scanner(System.in);
+		String password = s.nextLine();
+		
+		scholar.getCurrentUser().changePassword(password);
+		
+		System.out.println("Password successfully changed.");
+		
+		userMenu();
+	}
+	
+	private void searchPaper() {
+		System.out.println("Insert the author name:");
+		Scanner s = new Scanner(System.in);
+		String author = s.nextLine();
+		
+		Set result = scholar.getPapersFromAuthorName(author);
+		
+		List<Paper> list = new ArrayList<Paper>(result);
+		
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println((i + 1) + " - " + list.get(i));
+		}
+		System.out.println();
+		System.out.println("0 - Back");
+		
+		int choice = s.nextInt();
+		if (choice == 0)
+			userMenu();
+		else if (choice > 0 && choice <= list.size())
+			viewPaper(list.get(choice));
+		else
+			searchPaper();
+	}
+	
+	private void viewPaper(Paper paper) {
+		// TODO
+	}
+	
+	private void addPaper() {
 		// TODO
 	}
 }
